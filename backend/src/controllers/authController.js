9d84993907b2d557885f71e28bb31be8eda27a7f
unknown <piyushd2477@gmail.com>
2025-11-12 23:30:59 +0530

    changed some logic

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -13,8 +13,8 @@ export const register = async (req, res) => {
 
     const salt = await bcrypt.genSalt(10);
     const hashed = await bcrypt.hash(password, salt);
-    // Make every new user an admin by default
-    const user = await User.create({ name, email, password: hashed, role: 'admin' });
+    // New users default to 'user' role (as defined in User model)
+    const user = await User.create({ name, email, password: hashed });
     return res.status(201).json({ id: user._id, name: user.name, email: user.email });
   } catch (err) {
     return res.status(500).json({ message: 'Server error' });
